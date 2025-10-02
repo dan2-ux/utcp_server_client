53bdeb4b5a8c20406f101f3d7cf65344cc202b9c
dan2-ux <danhlanhat9@gmail.com>
2025-10-02 13:35:36 +0700

    Add files via upload

---
+++ example.py
@@ -0,0 +1,41 @@
+import asyncio
+from os import getcwd
+from utcp.utcp_client import UtcpClient
+from utcp.data.utcp_client_config import UtcpClientConfigSerializer
+
+async def main():
+    client: UtcpClient = await UtcpClient.create(
+        root_dir=getcwd(),
+        config=UtcpClientConfigSerializer().validate_dict(
+            {
+                "manual_call_templates": [
+                    {
+                        "name": " ",
+                        "call_template_type": "http",
+                        "http_method": "GET",
+                        "url": "http://localhost:8080/utcp2"
+                    }
+                ]
+            }
+        )
+    )
+
+    # List all available tools
+    print("Registered tools:")
+    tools = await client.search_tools("")
+    for tool in tools:
+        print(f" - {tool.name}")
+
+    # Call one of the tools
+    if tools:
+        tool_to_call = tools[0].name
+        args = {"body": {"value": "agree"}}
+
+        result = await client.call_tool(tool_to_call, args)
+        print(f"\nTool call result for '{tool_to_call}':")
+        print(result)
+    else:
+        print("No tools found.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ server.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from utcp_http.http_call_template import HttpCallTemplate
+from utcp.data.utcp_manual import UtcpManual
+from utcp.python_specific_tooling.tool_decorator import utcp_tool
+import uvicorn
+
+class TestRequest(BaseModel):
+    value: str
+
+__version__ = "1.0.0"
+BASE_PATH = "http://localhost:8080"
+
+app = FastAPI()
+
+@app.get("/utcp2", response_model=UtcpManual)
+def get_utcp():
+    return UtcpManual.create_from_decorators(manual_version=__version__) 
+
+@utcp_tool(tool_call_template=HttpCallTemplate(
+    name="test_provider",
+    url=f"{BASE_PATH}/test",
+    http_method="POST"
+))
+@app.post("/test")
+def test_endpoint(data: TestRequest):
+    return {"received": data.value}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8080)
